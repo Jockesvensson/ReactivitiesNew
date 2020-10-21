201fec817cfed99c44ace1a3122a3bb3eedb2ba2
Jockesvensson <jockesvensson1989@gmail.com>
2020-10-21 15:45:12 +0200

    End of section 20

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -3,6 +3,9 @@ export interface IProfile {
   username: string;
   bio: string;
   image: string;
+  following: Boolean;
+  followersCount: number;
+  followingCount: number;
   photos: IPhoto[];
 }
 
